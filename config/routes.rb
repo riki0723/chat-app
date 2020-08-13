60b0ec4b8009258cac7abd399796de308709082d
riki0723 <rikishibata0723@gmail.com>
2020-08-13 11:51:59 +0900

    roomsテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
- root "messages#index"
- resources :users, only: [:edit, :update]
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+ end
 end
